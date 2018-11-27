94fd77d224b4a1a7ee206ad4e5f90f4f71ef1342
huzh <huzhihai@njdhy.com.cn>
2018-11-27 18:48:24 +0800

    修改后台奥米巴字样文字标记

--- src/main/java/cn/com/njdhy/muscle/biceps/properties/SwaggerProperties.java
+++ src/main/java/cn/com/njdhy/muscle/biceps/properties/SwaggerProperties.java
@@ -3,7 +3,6 @@ package cn.com.njdhy.muscle.biceps.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 /**
